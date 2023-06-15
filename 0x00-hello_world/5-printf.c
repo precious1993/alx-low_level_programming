2bdfcccafec4275dce6bafab6e955e279a0c5a10
precious1993 <sinothandomqilingwa@gmail.com>
2023-06-15 12:02:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-printf("with proper gramar, but the outcome is a piece of art, \n"); 
-return (0);
+printf("with proper gramar, but the outcome is a piece of art,\n")
+return(0);
 }
