1b840a4b702cc47bd6afb63d7c8f0ab568afdefb
precious1993 <sinothandomqilingwa@gmail.com>
2023-07-11 05:48:39 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,6 +30,11 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
+		conct[i] = s1[i];
+		i++, ci++;
+	}
+	while (s2[ci] != '\0')
+	{
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
